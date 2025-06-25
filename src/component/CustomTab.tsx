0af252bd5cb504150a84e6b0b8dfa76c355714d0
Cody Cao <codykhoa123@gmail.com>
2025-06-25 08:47:41 -0700

    Added TicTacToe game
    Summary:
    
    Test Plan:
    
    ghstack-source-id: bda194a1dd434a0c7951ccde0be2d93dbc9e6647
    Pull Request resolved: https://github.com/khoacao-ccdk/Learning-React/pull/4

--- src/component/CustomTab.tsx
+++ src/component/CustomTab.tsx
@@ -1,37 +1,34 @@
-import React from "react"
-import { Tab, Typography } from "@mui/material"
-import { TabPanel, TabContext, TabList } from "@mui/lab"
+import React from "react";
+import { Tab, Typography } from "@mui/material";
+import { TabPanel, TabContext, TabList } from "@mui/lab";
 
 export type CustomTabProps = {
   data: Array<{
     label: string;
-    body: string
-  }>
-}
+    body: string;
+  }>;
+};
 
-export default function CustomTab({data}: CustomTabProps) {
-
+export default function CustomTab({ data }: CustomTabProps) {
   const [tab, setTab] = React.useState<number>(0);
 
-  function handleTabChange(e: React.SyntheticEvent, newValue: number) {
+  function handleTabChange(_e: React.SyntheticEvent, newValue: number) {
     setTab(newValue);
   }
 
   return (
-    <TabContext value={tab} >
+    <TabContext value={tab}>
       <TabList onChange={handleTabChange}>
-      {data.map((row, index) => (
-        <Tab key={index} value={index} label={row.label}/>
-      ))}
+        {data.map((row, index) => (
+          <Tab key={index} value={index} label={row.label} />
+        ))}
       </TabList>
 
       {data.map((row, index) => (
         <TabPanel key={index} value={index}>
-          <Typography>
-            {row.body}
-          </Typography>
+          <Typography>{row.body}</Typography>
         </TabPanel>
       ))}
     </TabContext>
-  )
+  );
 }
